36ad5e88f3f48f7dbba45083b5636c18207a1203
naosim <nao_pillows@yahoo.co.jp>
2015-05-05 16:41:02 +0900

    logを表示できる

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ var express = require('express'),
   RouterDoc = require('./lib/RouterDoc.js'),
   logger = require('./lib/logger.js'),
   setupAPI = require('./lib/usecase/SetupAPI.js'),
-  CycleLogCopy = require('./lib/CycleLogCopy.js');
+  CycleLogCopy = require('./lib/CycleLogCopy.js'),
+  BasicAuth = require('./lib/BasicAuth.js');
+
+var basicAuth = BasicAuth(function(id, pass) {
+  var date = new Date().getDate();
+  return id == 'user' + date && pass == 'pass' + date;
+});
+
+var test = function(req, res) {
+  res.send('GOOD');
+};
+
+var showLog = function(req, res) {
+  fs.readFile('./nohup.out', function(err, data) {
+    res.set('Content-Type', 'text/plain');
+    res.send(data);
+  });
+};
 
 var app = express();
 
@@ -24,12 +41,18 @@ app.use('*', function(req, res, next){
   getParseAction(req.headers['content-type']).apply(bodyParser, arguments);
 });
 
+var privateArea = function(req, res, next) { next(); };
+
 // API
 var routerDoc = RouterDoc(app, logger);
 var api = setupAPI();
-routerDoc.get( '/ctrl/reload', api.reloadAPITokenAPI.action, 'APITokenを更新する');
-routerDoc.post('/:api/:room' , api.sendMessageAPI.action   , 'POSTでメッセージを送信する');
-routerDoc.get( '/:api/:room' , api.sendMessageAPI.action   , 'GETでメッセージを送信する');
+routerDoc.get( '/ctrl/reload' , api.reloadAPITokenAPI.action, 'APITokenを更新する');
+routerDoc.get( '/private/*'   , basicAuth.valid(privateArea), 'BasicAuthが必要');
+routerDoc.get( '/private/test', test                        , 'Test用');
+routerDoc.get( '/private/log' , showLog                     , 'Log表示');
+routerDoc.post('/:api/:room'  , api.sendMessageAPI.action   , 'POSTでメッセージを送信する');
+routerDoc.get( '/:api/:room'  , api.sendMessageAPI.action   , 'GETでメッセージを送信する');
+
 routerDoc.listen(process.argv[2] || 3000);
 
 logger.log('## START CHATWORK PROXY ##');
